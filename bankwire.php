6b534130d6fcd47451e377b502d2495adbc45593
Michael Dekker <git@michaeldekker.nl>
2017-07-19 10:00:59 +0200

    Remove ps versions compliancy

--- bankwire.php
+++ bankwire.php
@@ -71,7 +71,6 @@ class BankWire extends PaymentModule
         $this->displayName = $this->l('Bank wire');
         $this->description = $this->l('Accept payments for your products via bank wire transfer.');
         $this->confirmUninstall = $this->l('Are you sure about removing these details?');
-        $this->ps_versions_compliancy = ['min' => '1.6', 'max' => '1.6.99.99'];
 
         if (!isset($this->owner) || !isset($this->details) || !isset($this->address)) {
             $this->warning = $this->l('Account owner and account details must be configured before using this module.');
