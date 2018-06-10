bc607daa64496d6bc22254523f96d3b84f613049
Markus Hitter <mah@jump-ing.de>
2018-06-10 15:26:21 +0200

    Make release 2.0.7.

--- bankwire.php
+++ bankwire.php
@@ -56,7 +56,7 @@ class BankWire extends PaymentModule
     {
         $this->name = 'bankwire';
         $this->tab = 'payments_gateways';
-        $this->version = '2.0.6';
+        $this->version = '2.0.7';
         $this->author = 'thirty bees';
         $this->need_instance = 1;
         $this->controllers = ['payment', 'validation'];
